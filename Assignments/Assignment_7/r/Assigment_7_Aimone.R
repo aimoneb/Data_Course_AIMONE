773728bd497aaf26d5a31c7ec2ba4ac111a3c2c8
aimoneb <behleeannaimone@yahoo.com>
2022-10-12 20:45:30 -0600

    assigmnet 7 commit

--- Assignments/Assignment_7/r/Assigment_7_Aimone.R
+++ Assignments/Assignment_7/r/Assigment_7_Aimone.R
@@ -6,7 +6,7 @@ dat<-read_csv("./data/Utah_Religions_by_County.csv")
 #this let me look at the data and see what was going on 
 view(dat)
 
-#this cleaned my data so that my variables were not spread out in multiple columns. I am assuming that the original vaues for each other these religions was a % of the religious population so I called it accordingly 
+#this cleaned my data so that my variables were not spread out in multiple columns. I am assuming that the original values for each other these religions was a % of the religious population so I called it accordingly 
 dat<-dat %>% 
   pivot_longer(c("Assemblies of God","Episcopal Church","Pentecostal Church of God","Greek Orthodox","LDS","Southern Baptist Convention","United Methodist Church","Buddhism-Mahayana","Catholic","Evangelical","Muslim","Non Denominational","Orthodox"),
                      names_to = "Religion",values_to = "Percent_by_religion") 
